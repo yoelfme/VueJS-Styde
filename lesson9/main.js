58fedb15aaefd872b9e3c412570f3fd84207e899
Yoel Monzon <yoelfme@hotmail.com>
2016-05-12 00:24:54 -0600

    [add] function to use filters

--- lesson9/main.js
+++ lesson9/main.js
@@ -1,19 +1,51 @@
+function findById(items, id) {
+    for (var i in items) {
+        if (items[i].id == id) {
+            return items[i];
+        }
+    }
+
+    return null;
+}
+
+Vue.filter('category', function (id) {
+    
+});
+
 var vm = new Vue({
     el: 'body',
     data: {
         notes: [
             {
                 note: 'Laravel 5.1 es LTS',
-                category: 'Laravel'
+                category_id: 1
             },
             {
                 note: 'Laravel 5.2 es la ultima version',
-                category: 'Laravel'
+                category_id: 1
             },
             {
                 note: '@click se utiliza como un alias de v-on:click',
-                category: 'Vue.js'
+                category_id: 2
+            }
+        ],
+        categories: [
+            {
+                id: 1,
+                name: 'Laravel'
+            },
+            {
+                id: 2,
+                name: 'Vue.js'
             }
         ]
+    },    
+    filters: {
+        category: function (id) {
+            var category =  findById(this.categories, id)
+
+
+            return category != null ? category.name : 'Sin categoria';
+        }
     }
 })
