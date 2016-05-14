b78a120d438a7d8e4164b6c37296998a56b6d7ed
Yoel Monzon <yoelfme@hotmail.com>
2016-05-14 00:21:55 -0600

    [add] component for note, class 10

--- lesson9/main.js
+++ lesson9/main.js
@@ -8,10 +8,37 @@ function findById(items, id) {
     return null;
 }
 
+Vue.filter('category', function (id) {
+    var category =  findById(this.categories, id)
+
+    return category != null ? category.name : 'Sin categoria';
+})
+
 Vue.component('select-category', {
     template: '#select_category_tpl',
     props: ['categories', 'id']
-})
+});
+
+Vue.component('note-row', {
+    template: '#note_row_tpl',
+    props: ['note', 'categories'],
+    data: function () {
+        return {
+            editing: false
+        };
+    },
+    methods: {
+        remove: function () {
+            this.$parent.notes.$remove(this.note);
+        },
+        edit: function () {
+            this.editing = true;
+        },
+        update: function () {
+            this.editing = false;
+        }
+    }
+});
 
 var vm = new Vue({
     el: 'body',
@@ -42,22 +69,7 @@ var vm = new Vue({
         ]
     },
     methods: {
-        deleteNote: function (note) {
-            this.notes.$remove(note);
-        },
-        editNote: function (note) {
-            Vue.set(note, 'editing', true);
-        },
-        updateNote: function (note) {
-            note.editing = false;
-        }
     },
     filters: {
-        category: function (id) {
-            var category =  findById(this.categories, id)
-
-
-            return category != null ? category.name : 'Sin categoria';
-        }
     }
 })
